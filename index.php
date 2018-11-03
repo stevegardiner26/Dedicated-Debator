5cf516a072725e2e8524dff3f83bcfb2cafc4c7f
Steve Gardiner <stevegardiner26@aol.com>
2018-11-03 14:23:38 -0400

    Test Commit

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: steve
+ * Dedicated Debator a Chat Service
+ * User: Steve Gardiner
  * Date: 11/3/2018
  * Time: 2:14 PM
  */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Dedicated Debator</title>
+
+    </head>
+    <body>
+        <h1>Dedicated Debator Test</h1>
+    </body>
+</html>
