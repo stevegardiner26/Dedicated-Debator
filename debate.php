0d12752b1f9afe38251063ead3cedfd0eaa1dadc
Steve Gardiner <stevegardiner26@aol.com>
2018-11-03 16:47:16 -0400

    Added Debate Page

--- debate.php
+++ debate.php
@@ -8,16 +8,11 @@
 ?>
 
 <?php include('html/nav.html')?>
-<section class="download bg-primary text-center" id="download">
+<section class="debateFinder text-center">
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2 class="section-heading">Discover different views asdasdasdaand perspectives!</h2>
-                <p>Find someone who opposes your views, and discuss with them to try and understand each other.</p>
-                <div class="badges">
-                    <a class="badge-link" href="#"><img src="../img/google-play-badge.svg" alt=""></a>
-                    <a class="badge-link" href="#"><img src="../img/app-store-badge.svg" alt=""></a>
-                </div>
+
             </div>
         </div>
     </div>
