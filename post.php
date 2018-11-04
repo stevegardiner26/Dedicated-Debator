fbf3e00580eb956af47aa4f00eef30400ec0d245
Steve Gardiner <stevegardiner26@aol.com>
2018-11-04 11:21:32 -0500

    Added dynamic Session Chat to post.php

--- post.php
+++ post.php
@@ -10,7 +10,7 @@ session_start();
 if(isset($_SESSION['name'])){
     $text = $_POST['text'];
 
-    $fp = fopen("log.html", 'a');
+    $fp = fopen("log" . $_SESSION['logger'] . ".html", 'a');
     fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
     fclose($fp);
 }
